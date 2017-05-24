73bfcf7364d22b2559b368d282caa4c56cce4659
celvins <vard-94@mail.ru>
2017-05-24 23:58:24 +0300

    whith wall.post function

---
+++ images.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+import vk
+import vk_auth
+client_id = '5732579'
+scope = 'wall'
+
+token, user_id = vk_auth.auth('79152986805', 'andranik111291a', client_id, scope)
+session = vk.Session(access_token=token) #ВОТ ТУТ ДОЛЖЕН БЫТЬ ТОКЕН
+api = vk.API(session)
+OWNER_ID = '53320096'
+href = 'photo26749604_456240990'
+r=api.users.get(user_ids = OWNER_ID, fields="can_post")
+messages = "Hi @username"
+messages = messages.replace('@username', r[0]['first_name'])
+print(messages)
+#messages = "Привет " + r[0]['first_name'] + ", я хочу поиграть с тобой в игру...."
+#can = r[0]["can_post"]
+#if can == 1:
+	#api.wall.post(owner_id=OWNER_ID, message=messages, attachments=href)
--- spam.py
+++ spam.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 from tkinter import *
+import vk
 import vk_auth
 
 def showMainPanel():
@@ -16,59 +17,47 @@ def showMainPanel():
             group_list.delete(0,END)
             group_count.config(text = 'Найдено ' + str(groups.pop(0)) + ' групп') #Удаляем первый элемент, в котором содержится количество найденных групп
             for i in groups:
-                group_list.insert(END,i['name'])
-
-                
+                group_list.insert(END,i['name'])             
     frame=Frame(mainPanel,bd=5)
     group_label = Label(mainPanel,text='Название группы', width=30)
     group = Entry(mainPanel, bd=2, text = 'Music')
     group_count = Label(frame,text='', width=30)
     group_list = Listbox(frame,height=10,width=50,selectmode=SINGLE)
     search = Button(mainPanel, text='Поиск', command = searchGroups)
-
-    
     scrollbar = Scrollbar(frame)
-    
     # первая привязка
     scrollbar['command'] = group_list.yview
     # вторая привязка
     group_list['yscrollcommand'] = scrollbar.set
-
-    
     group_label.pack()
     group.pack()
     search.pack()
     frame.pack()
     group_count.pack()
     scrollbar.pack(side='right', fill=Y)
-    group_list.pack()
-    
-    
+    group_list.pack()  
     mainPanel.mainloop()
-    
     auth_window.destroy()
-
-    
+    def send_messages(OWNER_ID, messages, href):
+        r=api.users.get(user_ids = OWNER_ID, fields="can_post")
+        messages = messages.replace('@username', r[0]['first_name'])
+        can = r[0]["can_post"]
+        if can == 1:
+            api.wall.post(owner_id=OWNER_ID, message=messages, attachments=href)
             
-
-
-
     
 auth_window = Tk()
 auth_window.title(u'Авторизация')
 auth_window.geometry('300x150+40+80')
-
 login_label = Label(auth_window,text='E-mail')
 login=Entry(auth_window, bd=2)
 password_label = Label(auth_window,text='Пароль')
 password=Entry(auth_window, bd=2, show='*')
 send = Button(auth_window, text='Войти', command = showMainPanel)
-
 login_label.pack()
 login.pack()
 password_label.pack()
 password.pack()
 send.pack()
-
 auth_window.mainloop()
 
