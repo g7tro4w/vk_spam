56efdc0b76ae59d43c4101faa4ff072ac69adec4
celvins <vard-94@mail.ru>
2017-05-25 21:12:13 +0300

    Update spam.py

--- spam.py
+++ spam.py
@@ -10,7 +10,7 @@ def showMainPanel():
     client_id = '5732579'
     scope = 'wall'
 
-    token, user_id = vk_auth.auth('79152986805','andranik111291a', client_id, scope)
+    token, user_id = vk_auth.auth('','', client_id, scope)
     session = vk.Session(access_token=token) #ВОТ ТУТ ДОЛЖЕН БЫТЬ ТОКЕН
     api = vk.API(session)
     def filterUsers(owner_id):
